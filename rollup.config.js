0c6c7efb0045a7976e32f4462df0b16f6ee38290
hophacker <j@paiyou.co>
2019-12-12 13:01:25 +0800

    lint fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import pkg from "./package.json"
 
 export default [
 	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: "src/main.js",
 		output: {
-			name: 'howLongUntilLunch',
+			name: "howLongUntilLunch",
 			file: pkg.browser,
-			format: 'umd'
+			format: "umd"
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
@@ -24,11 +24,11 @@ export default [
 	// an array for the `output` option, where we can specify 
 	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
-		external: ['ms'],
+		input: "src/main.js",
+		external: ["ms"],
 		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
+			{ file: pkg.main, format: "cjs" },
+			{ file: pkg.module, format: "es" }
 		]
 	}
-];
+]
